abd505b04ae7736399683e0d8c980e4975ce9422
mikel2026-max <mikelusidamen@gmail.com>
2026-01-20 17:44:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var mikel = document.getElementById("menulist")
+mikel.style.maxHeight ="0px" ;
+
+
+function toggleMenu(){
+    if(mikel.style.maxHeight =="0px")
+    {
+        mikel.style.maxHeight ="300px"
+    }
+
+    else{
+        mikel.style.maxHeight ="0px"
+    }
+
+}
+
